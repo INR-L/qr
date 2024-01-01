73d9d9642c43432dc639f1549a4a30f95455be34
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-01-01 11:46:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
-app.use('/pair', code);
+app.use('/code', code);
+app.use('pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
