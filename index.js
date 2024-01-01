43895cbe5f3119b0538d9d3ce9a8848c1a847ad6
inrl-md <109638623+inrl-md@users.noreply.github.com>
2024-01-01 11:45:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 let path = require('path')
-
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 let server = require('./qr'),
     code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
 app.use('/qr', server);
 app.use('/pair', code);
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
 	██████╗ ███████╗███████╗████████╗ █████╗ ██████╗ ██╗
