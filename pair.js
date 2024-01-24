d01873b9079db662c8e3ff4fb64e8d6ecf96f402
GURU SENSEI <sahoogurucharan00@gmail.com>
2024-01-24 17:58:11 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
             let session = makeWASocket({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "trace"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
